cd02ddd8525d71c785c89c763c55788f6ef40836
Jose Luis <joseluis27@ciencias.unam.mx>
2024-04-16 23:05:58 -0600

    Vector usuario

--- Funciones.py
+++ Funciones.py
@@ -1,4 +1,3 @@
-import numpy as np
 import re, random
 
 def leerBase():
@@ -6,7 +5,7 @@ def leerBase():
 	Funcion que leer una base de conocimiento y la transforma en un arreglo de filas
 	'''
 	datos = []
-	archivo = open('nombre archivo.txt', 'r')  # Reemplaza 'nombre_archivo.txt' con la ruta y nombre de tu archivo
+	archivo = open('recetas.csv', 'r')  # Reemplaza 'nombre_archivo.txt' con la ruta y nombre de tu archivo
 	try:
 		next(archivo)
 		for linea in archivo:
@@ -21,14 +20,70 @@ def cabeceras():
 	'''
 	Funcion que regresa la primera columna de un archivo de conocimiento.
 	'''
-	archivo = open('nombre archivo.txt', 'r')  # Reemplaza 'nombre_archivo.txt' con la ruta y nombre de tu archivo
+	archivo = open('recetas.csv', 'r')  # Reemplaza 'nombre_archivo.txt' con la ruta y nombre de tu archivo
 	try:
 		aux = archivo.readline().rstrip('\n').split(',')
 		aux = aux[1:]
 	finally:
 		archivo.close()
 	#print(aux)
-	return aux	
+	return aux
+
+def crearMensaje(opciones, inicio, fin):
+	mensaje = 'Cual de las siguientes opciones prefieres: \n'
+
+	for opcion in opciones:
+		if inicio == fin - 1:
+			mensaje += f'{inicio}:{opcion}.\n'
+		else:
+			mensaje += f'{inicio}:{opcion}, '
+		inicio += 1
+	return mensaje
+
+def validarOpcion(mensaje, minimo, maximo):
+	while True:
+		try:
+			opcion = int(input(mensaje))
+			if minimo <= opcion <= maximo:
+				return opcion
+			else:
+				print(f'El número debe estar entre {minimo} y {maximo}.')
+		except ValueError:
+			print('Ingresa un número entero.')
+
+def crearVectorUsuario():
+	cabecera = cabeceras()
+
+	proteinasReceta = crearMensaje( cabecera[:6], 0, 6 )
+	metodosCoccion = crearMensaje( cabecera[6:9], 6, 9 )
+	tiposReceta = crearMensaje( cabecera[9:12], 9, 12 )
+	utensilios =  crearMensaje( cabecera[12:17], 12, 17 )
+	complemento = crearMensaje( cabecera [17:], 17, 24 )
+	
+	vectorUsuario = [0] * (len(cabecera))
+
+	validacion = 0
+	while (validacion < 5):
+		try:
+			if validacion == 0:
+				opcionEleguida = validarOpcion(proteinasReceta, 0, 6)
+				vectorUsuario[opcionEleguida] = 1
+			if validacion == 1:
+				opcionEleguida = validarOpcion(metodosCoccion, 6, 9)
+				vectorUsuario[opcionEleguida] = 1
+			if validacion == 2:
+				opcionEleguida = validarOpcion(tiposReceta, 9, 12)
+				vectorUsuario[opcionEleguida] = 1
+			if validacion == 3:
+				opcionEleguida = validarOpcion(utensilios, 12, 17)
+				vectorUsuario[opcionEleguida] = 1
+			if validacion == 4:
+				opcionEleguida = validarOpcion(complemento, 17, 24)
+				vectorUsuario[opcionEleguida] = 1
+			validacion+=1
+		except ValueError:
+			print('Ingresa un número entero')
+	return vectorUsuario
 
 def obtenerDF(conocimiento):
 	return 0
@@ -59,3 +114,11 @@ def matchPreference(preferencia, conocimiento):
 	Calcular el angulo minimo entre el input, y la base de conocimiento
 	'''
 	return recomendado
+
+def main():
+	vector = crearVectorUsuario()
+	print(vector)
+	
+
+if __name__ == "__main__":
+    main()
